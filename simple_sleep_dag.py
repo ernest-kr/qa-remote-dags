97da8a20de5fc2384ab6d6c40aacef4cf4726dd4
Sanchit <sanchit.jha@astronomer.io>
2025-03-21 17:06:37 +0530

    updating bash to python operator

--- simple_sleep_dag.py
+++ simple_sleep_dag.py
@@ -1,19 +1,34 @@
-from airflow.models.dag import DAG
-from airflow.operators.bash import BashOperator
+from airflow import DAG
+from airflow.operators.python import PythonOperator
 from datetime import timedelta
 from airflow.utils import timezone
+import time
 
+# Define a simple function to sleep for 30 seconds
+def sleep_task():
+    print("Sleeping for 30 seconds...")
+    time.sleep(30)
+    print("Finished sleeping!")
+
+# Define default arguments
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": timezone.utcnow() - timedelta(days=1),
+    "retries": 1,
+}
+
+# Define the DAG
 with DAG(
     "simple_sleep_dag",
-    max_active_runs=3,
-    start_date=timezone.utcnow() - timedelta(days=1)
-) as dag_1:
-    tasks = [
-        BashOperator(
-            task_id="__".join(["tasks", "{}_of_{}".format(i, "10")]),
-            bash_command="echo test && sleep 30",
-        )
-        for i in range(1, 10 + 1)
-    ]
-    for i in range(1, len(tasks)):
-        tasks[i].set_upstream(tasks[(i - 1) // 2])
+    default_args=default_args,
+    description="A simple DAG that sleeps for 30 seconds",
+) as dag:
+
+    sleep_task_op = PythonOperator(
+        task_id="sleep_for_30_seconds",
+        python_callable=sleep_task,
+    )
+
+    sleep_task_op  # This sets the task in the DAG
+
