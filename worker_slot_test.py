fb8a01862f39853292336e21ef6efec383cc21dc
ernest-kr <4030180+ernest-kr@users.noreply.github.com>
2025-03-27 14:53:11 -0700

    Update worker_slot_test.py

--- worker_slot_test.py
+++ worker_slot_test.py
@@ -1,13 +1,12 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.utils.dates import days_ago
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 # Define default arguments for the DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': days_ago(1),
+    'start_date': datetime(2023, 10, 26), # or datetime.now()
     'retries': 0,  # No retries for testing
     'retry_delay': timedelta(minutes=5),
 }
@@ -25,7 +24,7 @@ with DAG(
     tasks = [
         BashOperator(
             task_id=f'sleep_task_{i}',
-            bash_command=f'sleep 300 && echo "Task {i} completed"', #sleep for an increasing amount of time.
+            bash_command=f'sleep {i * 10} && echo "Task {i} completed"', #sleep for an increasing amount of time.
         )
         for i in range(10)  # Adjust the number of tasks to test your worker slots
     ]
