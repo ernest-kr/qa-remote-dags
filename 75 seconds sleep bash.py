b1235787fa913644de62c15942d432e42fdfe03a
sreenusuuda <sreenivasulu.suuda@astronomer.io>
2025-03-30 22:20:42 +0530

    Update 75 seconds sleep bash.py

--- 75 seconds sleep bash.py
+++ 75 seconds sleep bash.py
@@ -7,13 +7,14 @@ with DAG(
     dag_id="bash_wait_dag",
     schedule=None,
     tags=["bash", "example"],
-    default_args={"retries": 1, "retry_delay": timedelta(seconds=5)},
 ) as dag:
     
     wait_task = BashOperator(
         task_id="wait_75_seconds",
         bash_command="sleep 75",
-        execution_timeout=timedelta(seconds=60)
+        execution_timeout=timedelta(seconds=60),
+        timeout=130,
+        retries=1,
     )
     
     wait_task
