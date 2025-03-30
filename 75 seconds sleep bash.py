1451b7264b1c24025f37c929d4882bfee893e8c9
sreenusuuda <sreenivasulu.suuda@astronomer.io>
2025-03-30 22:00:33 +0530

    Update 75 seconds sleep bash.py

--- 75 seconds sleep bash.py
+++ 75 seconds sleep bash.py
@@ -6,7 +6,8 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="bash_wait_dag",
     schedule=None,
-    tags=["bash", "example"]
+    tags=["bash", "example"],
+    default_args={"retries": 1, "retry_delay": timedelta(seconds=5)},
 ) as dag:
     
     wait_task = BashOperator(
