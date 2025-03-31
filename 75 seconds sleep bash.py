0243c54d6bf013c0002739a8217d02dce1a59969
sreenusuuda <sreenivasulu.suuda@astronomer.io>
2025-03-31 09:54:22 +0530

    Update 75 seconds sleep bash.py

--- 75 seconds sleep bash.py
+++ 75 seconds sleep bash.py
@@ -12,7 +12,7 @@ with DAG(
     wait_task = BashOperator(
         task_id="wait_75_seconds",
         bash_command="sleep 75",
-        execution_timeout=timedelta(seconds=60),
+        execution_timeout=timedelta(seconds=50),
     )
     
     wait_task
