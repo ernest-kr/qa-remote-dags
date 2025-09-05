0e21a6be96eccd410fd0d8824afa0162e70f7206
deeksha <deeksha.gupta@astronomer.io>
2025-09-05 23:55:42 +0530

    updated dags

--- custom_secret_remote.py
+++ custom_secret_remote.py
@@ -4,7 +4,7 @@ from airflow.models import Variable
 from datetime import datetime
 
 def fetch_secret():
-    val = Variable.get("test-remote-variable3")  # Will look for `airflow/test-variable` in AWS
+    val = Variable.get("test-remote-variable")  # Will look for `airflow/test-variable` in AWS
     print(f"Fetched secret value: {val}")
     if val != "super-secret-value":
         raise ValueError("Secret value mismatch!")
